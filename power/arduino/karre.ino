f4f54e46cb501930ee1310d102986781cb5b999e
Meno Abels <meno.abels@adviser.com>
2015-11-06 23:43:47 +0100

    initial

---
+++ power/arduino/karre.ino
@@ -0,0 +1,235 @@
+
+#include "Timer.h"
+
+#include "Wire.h"
+#include "Adafruit_MCP9808.h"
+
+
+
+class PortBase {
+  protected:
+    int8_t port;
+  public:
+  void setup(int8_t _port, int8_t mode) {
+    port = _port;
+    pinMode(_port, mode);
+  }
+};
+
+class AnalogRead : public PortBase {
+  private:
+    int16_t state;
+  public:
+    void setup(int8_t port) {
+      PortBase::setup(port, INPUT);
+      digitalWrite(port, HIGH); //pull on
+      state = 0;
+    }
+    int16_t read() {
+      state = analogRead(port);
+      return state;
+    }
+    int16_t currentValue() {
+      return state;
+    }
+};
+
+class DigitalWrite : public PortBase {
+  private:
+    int8_t state;
+  public:
+    void setup(int8_t port) {
+      PortBase::setup(port, OUTPUT);
+      state = LOW;
+    }
+
+    int8_t write(int8_t _state) {
+      state = _state;
+      digitalWrite(port, state);
+      return state;
+    }
+    int8_t currentValue() {
+      return state;
+    }
+};
+
+
+class Delegate
+{
+public:
+    typedef void (*Ptr)(void *);
+
+    template <class T, void (T::*TMethod)(void)>
+    static Ptr instance(T* object_ptr) {
+        return static_cast<Ptr>(&method_stub<T, TMethod>);
+    }
+private:
+    template <class T, void (T::*TMethod)(void)>
+    static void method_stub(void* object_ptr) {
+        T* p = static_cast<T*>(object_ptr);
+        return (p->*TMethod)(); // #2
+    }
+};
+
+class CarApp {
+  private:
+    const int8_t NEXT=42;
+    const int8_t DONE=0;
+    Timer t;
+
+    AnalogRead voltage_car;
+    AnalogRead voltage_220;
+    AnalogRead voltage_vbat;
+
+    DigitalWrite relay_car_220_loader;
+    DigitalWrite relay_vbat;
+    DigitalWrite relay_car;
+    DigitalWrite relay_inverter_net;
+
+    int16_t times;
+
+    Adafruit_MCP9808 tempsensor;
+    float temperatur;
+
+    int8_t  two_times;
+
+    int16_t readMCP9808() {
+//      tempsensor.shutdown_wake(0);
+//      temperatur = tempsensor.readTempC();
+//      tempsensor.shutdown_wake(1);
+    }
+
+    Delegate::Ptr delegateCheckPower;
+    Delegate::Ptr delegateCheckTemperatur;
+    Delegate::Ptr delegateSendState;
+
+  public:
+    CarApp() {
+      delegateCheckPower = Delegate::instance<CarApp, &CarApp::checkPower>(this);
+      delegateCheckTemperatur = Delegate::instance<CarApp, &CarApp::checkTemperatur>(this);
+      delegateSendState = Delegate::instance<CarApp, &CarApp::sendState>(this);
+    }
+
+    int setup() {
+      voltage_car.setup(A1);
+      voltage_220.setup(A2);
+      voltage_vbat.setup(A3);
+
+      relay_inverter_net.setup(1);
+      relay_car_220_loader.setup(2);
+      relay_vbat.setup(3);
+      relay_car.setup(4);
+
+      tempsensor.begin();
+      //Serial.println("jo-3");
+      t.every(1000, delegateSendState, this);
+      //delay(1000);
+      //Serial.println("jo-4");
+      sendState();
+    }
+    int loop() {
+      t.update();
+    }
+    void sendState() {
+      two_times = 0;
+
+      t.after(900, delegateCheckTemperatur, this);
+      t.after(200, delegateCheckPower, this);
+      Serial.print(F("{"));
+      Serial.print(F("instance:"));Serial.print((long)this);
+      Serial.print(F(",times:"));Serial.print(++times);
+      Serial.print(F(",millis:"));Serial.print(millis());
+      Serial.print(F(",temperatur:"));Serial.print(temperatur);
+      Serial.print(F(",voltage_220:"));Serial.print(voltage_220.currentValue());
+      Serial.print(F(",voltage_car:"));Serial.print(voltage_car.currentValue());
+      Serial.print(F(",voltage_vbat:"));Serial.print(voltage_vbat.currentValue());
+      Serial.print(F(",relay_car_220_loader:"));Serial.print(relay_car_220_loader.currentValue());
+      Serial.print(F(",relay_vbat:"));Serial.print(relay_inverter_net.currentValue());
+      Serial.print(F(",relay_car:"));Serial.print(relay_car.currentValue());
+      Serial.print(F(",relay_inverter:"));Serial.print(relay_inverter_net.currentValue());
+      Serial.print(F("}\n"));
+    }
+
+    void checkPower() {
+      if (two_times++ < 1) {
+        t.after(400, delegateCheckPower, this);
+      }
+      runVoltage220() && readVoltageCar() && readVoltageVBat();
+    }
+
+    void checkTemperatur() {
+      temperatur = readMCP9808();
+      if (temperatur < 1000) {
+        // pwm off
+      } else if (temperatur < 2000) {
+        // pwm 40%
+      } else if (temperatur < 3000) {
+        // pwm 80%
+      } else if (temperatur < 3000) {
+        // pwm 100%
+      } else if (temperatur > 3200) {
+        // poweroff
+      }
+    }
+
+    int runVoltage220() {
+      int16_t val = voltage_220.read();
+      if (val <= 2000) return NEXT;
+      relay_car_220_loader.write(HIGH);
+      relay_inverter_net.write(LOW);
+      relay_car.write(LOW);
+      relay_vbat.write(LOW);
+      return DONE;
+    }
+
+    int readVoltageCar() {
+      int16_t val = voltage_car.read();
+      if (val <= 2000) return NEXT;
+      relay_car_220_loader.write(LOW);
+      relay_inverter_net.write(HIGH);
+      relay_vbat.write(LOW);
+      relay_car.write(HIGH);
+      return DONE;
+    }
+
+    int readVoltageVBat() {
+      int16_t val = voltage_vbat.read();
+      if (val <= 2000) {
+        // poweroff ?;
+        relay_car_220_loader.write(LOW);
+        relay_inverter_net.write(LOW);
+        relay_vbat.write(LOW);
+        relay_car.write(LOW);
+        return NEXT;
+      }
+      relay_car_220_loader.write(LOW);
+      relay_inverter_net.write(HIGH);
+      relay_vbat.write(HIGH);
+      relay_car.write(LOW);
+      return DONE;
+    }
+};
+
+CarApp app;
+
+//Timer t;
+void loop()
+{
+  app.loop();
+//  t.update();
+}
+
+//int cnt = 0;
+//void jo() {
+//  Serial.println((int)&app);
+//}
+void setup()
+{
+  Serial.begin(9600);
+
+//  t.every(1000, jo);
+
+//  delay(2000);
+//  Serial.println("jo-1");
+  app.setup();
+}
